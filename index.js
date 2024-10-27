d8d73b7b3aca68658eebeb672a0fa227a28839fb
Ryan Bjorkman <ryanbjorkman@icloud.com>
2024-10-27 10:02:46 +0200

    added conversions for consistancey and made use of lambda function for unit conversions ans error handling

--- index.js
+++ index.js
@@ -13,19 +13,39 @@ const initialDistanceKm = 0; // distance (km)
 const initialFuelKg = 5000; // remaining fuel (kg)
 const fuelBurnRateKgs = 0.5; // fuel burn rate (kg/s)
 
+// Convert acceleration from m/s^2 to km/h^2
+const accelerationKmH2 = accelerationMs2 * 12960; // 1 m/s^2 = 12960 km/h^2
 
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+
+const calcNewDistance = initialDistanceKm + (velocityKmH*(timeInSeconds/3600)); //calcultes new distance
+const calcRemainingFuelKg = initialFuelKg - (fuelBurnRateKgs * timeInSeconds); //calculates remaining fuel
+const currentVelocityKmH = velocityKmH + (accelerationKmH2 * (timeInSeconds / 3600)) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
+// calcNewVel = (vel, acc, time) => { 
+//   return vel + (acc*time)
+// }
+
+function calcNewVelocityKmH(velocityKmH, accelerationMs2, timeInSeconds){
+  // check if the parameters are not negative
+  if(velocityKmH < 0 || accelerationMs2 < 0 || timeInSeconds < 0){
+    throw new Error('Parameters cannot be negative')
+  }
+
+  // convert acceleration from m/s^2 to km/h^2
+  const accelerationKmH2 = accelerationMs2 * 12960; // 1 m/s^2 = 12960 km/h^2
+  const timeInHours = timeInSeconds / 3600
+  return velocityKmH + (accelerationKmH2 * timeInHours)
 }
 
-console.log(`Corrected New Velocity: ${vel2} km/h`);
-console.log(`Corrected New Distance: ${d2} km`);
-console.log(`Corrected Remaining Fuel: ${rf} kg`);
+try {
+  const currentVelocityKmH = calcNewVelocityKmH(velocityKmH, accelerationMs2, timeInSeconds)
+  console.log(`Corrected New Velocity: ${currentVelocityKmH} km/h`);
+  console.log(`Corrected New Distance: ${calcNewDistance} km`);
+  console.log(`Corrected Remaining Fuel: ${calcRemainingFuelKg} kg`);
+} catch (error) {
+  console.log(error.message)
+}
 
 
 
